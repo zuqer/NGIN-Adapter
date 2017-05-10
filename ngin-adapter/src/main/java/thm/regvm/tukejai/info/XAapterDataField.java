f134ae39d3a3100e979aed874de8bfbd05a52d2c
Witsanu <Witsanu@gmail.com>
2017-05-10 17:34:53 +0700

    add model information

--- ngin-adapter/src/main/java/thm/regvm/tukejai/info/XAapterDataField.java
+++ ngin-adapter/src/main/java/thm/regvm/tukejai/info/XAapterDataField.java
@@ -1,7 +1,10 @@
 package thm.regvm.tukejai.info;
 
 public class XAapterDataField {
-	
-	public static String[] fieldArray = {""} ;
-	
+
+	public static String[] fieldArray = { "post_title", "post_name", "ID", "post_excerpt", "post_content", "post_status", "menu_order", "post_date", "post_author", "comment_status", "sku",
+			"downloadable", "virtual", "stock", "regular_price", "sale_price", "weight", "length", "width", "height", "tax_class", "visibility", "stock_status", "backorders", "manage_stock",
+			"tax_status", "upsell_ids", "crosssell_ids", "featured", "sale_price_dates_from", "sale_price_dates_to", "download_limit", "download_expiry", "product_url", "button_text", "images",
+			"downloadable_files", "tax:manufacturer", "tax:product_type", "tax:product_visibility", "tax:product_cat", "tax:product_tag", "tax:product_shipping_class" };
+
 }
--- ngin-adapter/src/main/java/thm/regvm/tukejai/info/XAdapterDataImportInfo.java
+++ ngin-adapter/src/main/java/thm/regvm/tukejai/info/XAdapterDataImportInfo.java
@@ -2,4 +2,392 @@ package thm.regvm.tukejai.info;
 
 public class XAdapterDataImportInfo {
 
+	private String postTitile;
+	private String postName;
+	private String ID;
+	private String postExcerpt;
+	private String postContent;
+	private String postStatus;
+	private String menuOrder;
+	private String postDate;
+	private String postAuthor;
+	private String commentStatus;
+	private String sku;
+	private String downloadable;
+	private String virtual;
+	private String stock;
+	private String regularPrice;
+	private String salePrice;
+	private String weight;
+	private String length;
+	private String width;
+	private String height;
+	private String taxClass;
+	private String visibility;
+	private String stockStatus;
+	private String backorders;
+	private String manageStock;
+	private String taxStatus;
+	private String upsellIds;
+	private String crosssellIds;
+	private String featured;
+	private String salePriceDatesFrom;
+	private String salePriceDatesTo;
+	private String downloadLimit;
+	private String downloadExpiry;
+	private String productUrl;
+	private String buttonText;
+	private String images;
+	private String downloadableFiles;
+	private String taxManufacturer;
+	private String taxProductType;
+	private String taxProductVisibility;
+	private String taxProductCat;
+	private String taxProductTag;
+	private String taxProduct_shippingClass;
+
+	public String getPostTitile() {
+		return postTitile;
+	}
+
+	public void setPostTitile(String postTitile) {
+		this.postTitile = postTitile;
+	}
+
+	public String getPostName() {
+		return postName;
+	}
+
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getPostExcerpt() {
+		return postExcerpt;
+	}
+
+	public void setPostExcerpt(String postExcerpt) {
+		this.postExcerpt = postExcerpt;
+	}
+
+	public String getPostContent() {
+		return postContent;
+	}
+
+	public void setPostContent(String postContent) {
+		this.postContent = postContent;
+	}
+
+	public String getPostStatus() {
+		return postStatus;
+	}
+
+	public void setPostStatus(String postStatus) {
+		this.postStatus = postStatus;
+	}
+
+	public String getMenuOrder() {
+		return menuOrder;
+	}
+
+	public void setMenuOrder(String menuOrder) {
+		this.menuOrder = menuOrder;
+	}
+
+	public String getPostDate() {
+		return postDate;
+	}
+
+	public void setPostDate(String postDate) {
+		this.postDate = postDate;
+	}
+
+	public String getPostAuthor() {
+		return postAuthor;
+	}
+
+	public void setPostAuthor(String postAuthor) {
+		this.postAuthor = postAuthor;
+	}
+
+	public String getCommentStatus() {
+		return commentStatus;
+	}
+
+	public void setCommentStatus(String commentStatus) {
+		this.commentStatus = commentStatus;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getDownloadable() {
+		return downloadable;
+	}
+
+	public void setDownloadable(String downloadable) {
+		this.downloadable = downloadable;
+	}
+
+	public String getVirtual() {
+		return virtual;
+	}
+
+	public void setVirtual(String virtual) {
+		this.virtual = virtual;
+	}
+
+	public String getStock() {
+		return stock;
+	}
+
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+
+	public String getRegularPrice() {
+		return regularPrice;
+	}
+
+	public void setRegularPrice(String regularPrice) {
+		this.regularPrice = regularPrice;
+	}
+
+	public String getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(String salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public String getLength() {
+		return length;
+	}
+
+	public void setLength(String length) {
+		this.length = length;
+	}
+
+	public String getWidth() {
+		return width;
+	}
+
+	public void setWidth(String width) {
+		this.width = width;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getTaxClass() {
+		return taxClass;
+	}
+
+	public void setTaxClass(String taxClass) {
+		this.taxClass = taxClass;
+	}
+
+	public String getVisibility() {
+		return visibility;
+	}
+
+	public void setVisibility(String visibility) {
+		this.visibility = visibility;
+	}
+
+	public String getStockStatus() {
+		return stockStatus;
+	}
+
+	public void setStockStatus(String stockStatus) {
+		this.stockStatus = stockStatus;
+	}
+
+	public String getBackorders() {
+		return backorders;
+	}
+
+	public void setBackorders(String backorders) {
+		this.backorders = backorders;
+	}
+
+	public String getManageStock() {
+		return manageStock;
+	}
+
+	public void setManageStock(String manageStock) {
+		this.manageStock = manageStock;
+	}
+
+	public String getTaxStatus() {
+		return taxStatus;
+	}
+
+	public void setTaxStatus(String taxStatus) {
+		this.taxStatus = taxStatus;
+	}
+
+	public String getUpsellIds() {
+		return upsellIds;
+	}
+
+	public void setUpsellIds(String upsellIds) {
+		this.upsellIds = upsellIds;
+	}
+
+	public String getCrosssellIds() {
+		return crosssellIds;
+	}
+
+	public void setCrosssellIds(String crosssellIds) {
+		this.crosssellIds = crosssellIds;
+	}
+
+	public String getFeatured() {
+		return featured;
+	}
+
+	public void setFeatured(String featured) {
+		this.featured = featured;
+	}
+
+	public String getSalePriceDatesFrom() {
+		return salePriceDatesFrom;
+	}
+
+	public void setSalePriceDatesFrom(String salePriceDatesFrom) {
+		this.salePriceDatesFrom = salePriceDatesFrom;
+	}
+
+	public String getSalePriceDatesTo() {
+		return salePriceDatesTo;
+	}
+
+	public void setSalePriceDatesTo(String salePriceDatesTo) {
+		this.salePriceDatesTo = salePriceDatesTo;
+	}
+
+	public String getDownloadLimit() {
+		return downloadLimit;
+	}
+
+	public void setDownloadLimit(String downloadLimit) {
+		this.downloadLimit = downloadLimit;
+	}
+
+	public String getDownloadExpiry() {
+		return downloadExpiry;
+	}
+
+	public void setDownloadExpiry(String downloadExpiry) {
+		this.downloadExpiry = downloadExpiry;
+	}
+
+	public String getProductUrl() {
+		return productUrl;
+	}
+
+	public void setProductUrl(String productUrl) {
+		this.productUrl = productUrl;
+	}
+
+	public String getButtonText() {
+		return buttonText;
+	}
+
+	public void setButtonText(String buttonText) {
+		this.buttonText = buttonText;
+	}
+
+	public String getImages() {
+		return images;
+	}
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+
+	public String getDownloadableFiles() {
+		return downloadableFiles;
+	}
+
+	public void setDownloadableFiles(String downloadableFiles) {
+		this.downloadableFiles = downloadableFiles;
+	}
+
+	public String getTaxManufacturer() {
+		return taxManufacturer;
+	}
+
+	public void setTaxManufacturer(String taxManufacturer) {
+		this.taxManufacturer = taxManufacturer;
+	}
+
+	public String getTaxProductType() {
+		return taxProductType;
+	}
+
+	public void setTaxProductType(String taxProductType) {
+		this.taxProductType = taxProductType;
+	}
+
+	public String getTaxProductVisibility() {
+		return taxProductVisibility;
+	}
+
+	public void setTaxProductVisibility(String taxProductVisibility) {
+		this.taxProductVisibility = taxProductVisibility;
+	}
+
+	public String getTaxProductCat() {
+		return taxProductCat;
+	}
+
+	public void setTaxProductCat(String taxProductCat) {
+		this.taxProductCat = taxProductCat;
+	}
+
+	public String getTaxProductTag() {
+		return taxProductTag;
+	}
+
+	public void setTaxProductTag(String taxProductTag) {
+		this.taxProductTag = taxProductTag;
+	}
+
+	public String getTaxProduct_shippingClass() {
+		return taxProduct_shippingClass;
+	}
+
+	public void setTaxProduct_shippingClass(String taxProduct_shippingClass) {
+		this.taxProduct_shippingClass = taxProduct_shippingClass;
+	}
+
 }
